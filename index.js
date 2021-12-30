d3a9e2923422c8ab51b4b5b4b0a877d8620f5ba9
RuthTadesse <tib6297@gmail.com>
2021-12-30 14:08:09 -0500

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,36 @@
-// Code your solutions in this file
+
+const name = ['Lisa', 'Kaitlin','Jan'];
+const event = 'surprise';
+
+function writeCard() {
+  for (let i = 0; i < name.length; i++)
+  return 'Thank you, ${name[i]}', for the wonderful ${event} gift!'
+};'
+
+
+function wrapGifts(gifts) {
+  for (let i = 0; i < gifts.length; i++) {
+    console.log(`Wrapped ${gifts[i]} and added a bow!`);
+    debugger;
+  }
+
+  return gifts;
+}
+
+wrapGifts(gifts);
+
+
+
+
+let count = 1;
+while (count < 5) {
+  debugger;
+  if (count === 1) {
+    console.log(`This loop has executed ${count} time.`);
+  } else {
+    console.log(`This loop has executed ${count} times.`);
+  }
+  count += 1;
+  debugger;
+}
+
