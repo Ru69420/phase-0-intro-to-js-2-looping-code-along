06f3e7b83e51347107c0a6336a1faa3103b7c1f4
RuthTadesse <tib6297@gmail.com>
2022-02-15 09:12:21 -0500

    commit tree 3abd8c6bad4f0beefa134ff9a56ba533ef2d4b95

--- index.js
+++ index.js
@@ -2,35 +2,44 @@
 const name = ['Lisa', 'Kaitlin','Jan'];
 const event = 'surprise';
 
-function writeCard() {
-  for (let i = 0; i < name.length; i++)
-  return 'Thank you, ${name[i]}', for the wonderful ${event} gift!'
-};'
+// function writeCard() {
+//   for (let i = 0; i < name.length; i++)
+//   return 'Thank you, ${name[i]}', for the wonderful ${event} gift!'
+// };'
+
+function writeCard(){
+  
+}
+
+
+
 
 
 function wrapGifts(gifts) {
   for (let i = 0; i < gifts.length; i++) {
     console.log(`Wrapped ${gifts[i]} and added a bow!`);
-    debugger;
+    
   }
 
   return gifts;
 }
 
-wrapGifts(gifts);
 
 
 
 
+function countDown (){
 let count = 1;
 while (count < 5) {
-  debugger;
+  
   if (count === 1) {
     console.log(`This loop has executed ${count} time.`);
   } else {
     console.log(`This loop has executed ${count} times.`);
   }
   count += 1;
-  debugger;
+  
+}
+
 }
 
